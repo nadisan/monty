4fc8109e7da5c5aa10754ffba561b53d93355d3b
Nadia <nadiawellela@gmail.com>
2023-06-20 22:58:37 -0700

    errors

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		pFile = fopen(argv[1], "r");
 	if (pFile == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		/*fprintf(stderr, "Error: Can't open file %s\n", argv[1]);*/
 		exit(EXIT_FAILURE);
 	}
 	else
