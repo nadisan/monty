e198c9c90a6985cd677a479317085ac59080708b
Nadia <nadiawellela@gmail.com>
2023-06-20 23:23:18 -0700

    pall

--- main.c
+++ main.c
@@ -5,6 +5,52 @@
 
 
 /**
+ * push - add int to stack as header
+ * @stack: stack linked list
+ * @line_number: number to insert on stack
+ *
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	int value = line_number;
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
+
+/**
+ * pall - prints all stack elements
+ * @stack: stack linked list
+ * @line_number: number to insert on stack
+ *
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	unsigned int i = 0;
+
+	while (current != NULL && i <= line_number)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+}
+
+
+/**
  * main- executes command from argument
  * @argc: number of command
  * @argv: operation and command file
@@ -16,7 +62,7 @@ int main(int argc, char *argv[])
 {
 	FILE *pFile;
 	char buffer[100];
-	/*stack_t *stack = NULL;*/
+	stack_t *stack = NULL;
 	/*char *opcode = NULL;	*/
 	unsigned int line_number = 0;
 	char *fun;
@@ -40,7 +86,11 @@ int main(int argc, char *argv[])
 			if (fgets(buffer, 100, pFile) == NULL)
 				break;
 			fun = strtok(buffer, " \n");
-			printf("%s", fun);
+
+			if (strcmp(fun, "push") == 0)
+				push(&stack, line_number);
+			if (strcmp(fun, "pall") == 0)
+				pall(&stack, line_number);
 		}
 		fclose(pFile);
 	}
