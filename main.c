981f6f31d596d6ee83988a9f61deaf75597cbda3
Nadia <nadiawellela@gmail.com>
2023-06-20 22:57:26 -0700

    errors

--- main.c
+++ main.c
@@ -13,10 +13,17 @@ int main(int argc, char *argv[])
 	unsigned int line_number = 0;
 	char *fun;
 
-	if (argc == 2)
+	if (argc != 2)
+	{	perror("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	else
 		pFile = fopen(argv[1], "r");
 	if (pFile == NULL)
-		perror("Error opening file");
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	else
 	{
 		while (!feof(pFile))
@@ -25,7 +32,6 @@ int main(int argc, char *argv[])
 			if (fgets(buffer, 100, pFile) == NULL)
 				break;
 			fun = strtok(buffer, " \n");
-			printf("%s", fun);
 			/*if (strcmp(fun, "push") == 0;
 				push(&stack, line_number);
 			if (strcmp(fun, "pall") == 0;
