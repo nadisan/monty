6df96f29a242c15e5a9c107be5c1f2f0d2972ed0
Nadia <nadiawellela@gmail.com>
2023-06-21 08:31:45 -0700

    main.c

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 #include <string.h>
 #include "monty.h"
 #include <stdlib.h>
+#include <ctype.h>
+
+void fre(stack_t *stack)
+{
+stack_t *temp = NULL;
+
+while (stack)
+{
+temp = stack;
+stack = stack->next;
+free(temp);
+}
+stack = NULL;
+}
+
 
 /**
  * ex_inst - prints all stack elements
@@ -12,7 +27,7 @@
  *
  */
 
-void ex_inst(instruction_t *in, char *buf, unsigned int l_num, stack_t **stack)
+int ex_inst(instruction_t *in, char *buf, unsigned int l_num, stack_t **stack)
 {
 	unsigned int i = 0, j = 0;
 	char *fun[3];
@@ -30,20 +45,23 @@ void ex_inst(instruction_t *in, char *buf, unsigned int l_num, stack_t **stack)
 		if (strcmp(in[i].opcode, fun[0]) == 0)
 		{
 
-			if (i == 0 && j != 2)
+			if ((i == 0 && j != 2 ) || ((i == 0) && !isdigit(fun[1][0])))
 			{
-				{	fprintf(stderr, "L%u: usage: push integer\n", l_num);
-					exit(EXIT_FAILURE);
+				{
+					fprintf(stderr, "L%u: usage: push integer\n", l_num);
+					exit(EXIT_FAILURE);		
 				}
 			}
 			in[i].f(stack, l_num);
-			return;
+			return (0);
 		}
 		i++;
 	}
-	fprintf(stderr, "L%u: unknown instruction %s\n", l_num, fun[1]);
-	exit(EXIT_FAILURE);
+	fprintf(stderr, "L%u: unknown instruction %s\n", l_num, fun[0]);
+	fre(*stack);
+	return(1);
 }
+
 
 /**
  * main- executes command from argument
@@ -58,9 +76,9 @@ int main(int argc, char *argv[])
 	FILE *pFile;
 	char buf[100];
 	stack_t *stack = NULL;
-	stack_t *temp = NULL;
+
 	/*char *opcode = NULL;	*/
-	unsigned int l_num = 0;
+	unsigned int l_num = 1;
 
 	instruction_t in[] = {
 		{"push", push},
@@ -77,20 +95,15 @@ int main(int argc, char *argv[])
 	{	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	else
+	while (!feof(pFile))
 	{
-		while (!feof(pFile))
-		{	l_num++;
-			if (fgets(buf, 100, pFile) == NULL)
-				break;
+		while (fgets(buf, 100, pFile))
+		{
 			ex_inst(in, buf, l_num, &stack);
+			l_num++;
 		}
-		fclose(pFile);
 	}
-	while (stack)
-	{	temp = stack;
-		stack = stack->next;
-		free(temp);
-	}
+	fclose(pFile);
+	fre(stack);
 	return (0);
 }
