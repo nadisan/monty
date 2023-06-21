bb2569799e2014f4cc05b05ef060a3eb6c84b2b0
Nadia <nadiawellela@gmail.com>
2023-06-21 06:30:58 -0700

    main.c

--- main.c
+++ main.c
@@ -3,88 +3,48 @@
 #include "monty.h"
 #include <stdlib.h>
 
-
 /**
- * push - add int to stack as header
+ * ex_inst - prints all stack elements
+ * @in: instruction_t
  * @stack: stack linked list
- * @line_number: number to insert on stack
+ * @l_num: number to insert on stack
+ * @buf: line from file
  *
  */
 
-void push(stack_t **stack, unsigned int line_number)
+void ex_inst(instruction_t *in, char *buf, unsigned int l_num, stack_t **stack)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
-
-
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = line_number;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
-}
-
-/**
- * pall - prints all stack elements
- * @stack: stack linked list
- * @line_number: number to insert on stack
- *
- */
-
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
-
-
-	while (current != NULL && line_number != 0)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
-}
-
-void exec_inst(instruction_t *instructions, char *buffer, unsigned int line_number, stack_t **stack)
-{
-	int i = 0, j = 0;
+	unsigned int i = 0, j = 0;
 	char *fun[3];
 
-	fun[j] = strtok(buffer, " \n");
-	
+	fun[j] = strtok(buf, " \n");
+
 	while (fun[j] && j < 2)
-	{	
+	{
 		j++;
 		fun[j] = strtok(NULL, " \n");
 	}
-	
-	while (instructions[i].opcode != NULL)
+
+	while (in[i].opcode != NULL)
 	{
-		if (strcmp(instructions[i].opcode, fun[0]) == 0)
+		if (strcmp(in[i].opcode, fun[0]) == 0)
 		{
 
 			if (i == 0 && j != 2)
 			{
-				{	fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                                	exit(EXIT_FAILURE);
+				{	fprintf(stderr, "L%u: usage: push integer\n", l_num);
+					exit(EXIT_FAILURE);
 				}
 			}
-			instructions[i].f(stack, line_number);
+			in[i].f(stack, l_num);
 			return;
 		}
 		i++;
 	}
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, fun[1]);
+	fprintf(stderr, "L%u: unknown instruction %s\n", l_num, fun[1]);
 	exit(EXIT_FAILURE);
 }
-instruction_t instructions[] = {
-    {"push", push},
-    {"pall", pall},
-    {NULL, NULL}
-};
+
 /**
  * main- executes command from argument
  * @argc: number of command
@@ -96,13 +56,17 @@ instruction_t instructions[] = {
 int main(int argc, char *argv[])
 {
 	FILE *pFile;
-	char buffer[100];
+	char buf[100];
 	stack_t *stack = NULL;
 	stack_t *temp = NULL;
 	/*char *opcode = NULL;	*/
-	unsigned int line_number = 0;
+	unsigned int l_num = 0;
 
-
+	instruction_t in[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
 	if (argc != 2)
 	{	fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
@@ -110,35 +74,21 @@ int main(int argc, char *argv[])
 	else
 		pFile = fopen(argv[1], "r");
 	if (pFile == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	{	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		while (!feof(pFile))
-		{
-			line_number++;
-			if (fgets(buffer, 100, pFile) == NULL)
+		{	l_num++;
+			if (fgets(buf, 100, pFile) == NULL)
 				break;
-
-			exec_inst(instructions, buffer, line_number, &stack);
-			/*if (strcmp(fun, "push") == 0)
-			*	push(&stack, line_number);
-			*else if (strcmp(fun, "pall") == 0)
-			*	pall(&stack, line_number);
-			* else
-			* {
-			*	fprintf(stderr, "L%d: unknown instruction %s,\n", line_number, fun);
-			*	exit(EXIT_FAILURE);
-			*}
-			*/
+			ex_inst(in, buf, l_num, &stack);
 		}
 		fclose(pFile);
 	}
 	while (stack)
-	{
-		temp = stack;
+	{	temp = stack;
 		stack = stack->next;
 		free(temp);
 	}
--- monty.h
+++ monty.h
@@ -35,4 +35,8 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+
+
 #endif
